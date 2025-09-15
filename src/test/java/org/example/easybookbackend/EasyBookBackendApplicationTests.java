03dacadaabb735029db89ff8de8919280d61668a
DarwinSt <darwin_moraga@fet.edu.co>
2025-09-15 10:31:45 -0500

    Inicio EasyBook

---
+++ src/test/java/org/example/easybookbackend/EasyBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.easybookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
